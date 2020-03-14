b644a4d079f0be15ea03f46f5e5736771e1bdfa9
vdancik <vdancik@users.noreply.github.com>
2020-03-13 20:09:23 -0400

    implement DrugCentral transformer

--- transformers/drugcentral/python-flask-server/openapi_server/__main__.py
+++ transformers/drugcentral/python-flask-server/openapi_server/__main__.py
@@ -4,14 +4,14 @@ import connexion
 
 from openapi_server import encoder
 
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'Transformer API for DrugCentral'},
+            pythonic_params=True)
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Transformer API for DrugCentral'},
-                pythonic_params=True)
-    app.run(port=8080)
+    app.run(port=8260)
 
 
 if __name__ == '__main__':
