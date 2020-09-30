2dba723cfa6604771ee1d5d5d7ab7a62ff5d8fce
vdancik <vdancik@broadinstitute.org>
2020-09-29 21:26:52 -0400

    autogenerate python flask server for CMAP version 2.1

--- transformers/cmap/python-flask-server/openapi_server/controllers/transformers_controller.py
+++ transformers/cmap/python-flask-server/openapi_server/controllers/transformers_controller.py
@@ -1,42 +1,26 @@
 import connexion
 import six
 
-from openapi_server.models.compound_info import CompoundInfo  # noqa: E501
+from openapi_server.models.element import Element  # noqa: E501
 from openapi_server.models.error_msg import ErrorMsg  # noqa: E501
-from openapi_server.models.gene_info import GeneInfo  # noqa: E501
 from openapi_server.models.transformer_info import TransformerInfo  # noqa: E501
 from openapi_server.models.transformer_query import TransformerQuery  # noqa: E501
 from openapi_server import util
 
 
-def input_class_compound_transform_post(input_class, transformer_query):  # noqa: E501
+def input_class_output_class_transform_post(input_class, output_class, transformer_query):  # noqa: E501
     """Transform a list of genes or compounds
 
     Depending on the function of a transformer, creates, expands, or filters a list. # noqa: E501
 
     :param input_class: input class for the transformer
     :type input_class: str
+    :param output_class: output class for the transformer
+    :type output_class: str
     :param transformer_query: transformer query
     :type transformer_query: dict | bytes
 
-    :rtype: List[CompoundInfo]
-    """
-    if connexion.request.is_json:
-        transformer_query = TransformerQuery.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
-
-
-def input_class_gene_transform_post(input_class, transformer_query):  # noqa: E501
-    """Transform a list of genes or compounds
-
-    Depending on the function of a transformer, creates, expands, or filters a list. # noqa: E501
-
-    :param input_class: input class for the transformer
-    :type input_class: str
-    :param transformer_query: transformer query
-    :type transformer_query: dict | bytes
-
-    :rtype: List[GeneInfo]
+    :rtype: List[Element]
     """
     if connexion.request.is_json:
         transformer_query = TransformerQuery.from_dict(connexion.request.get_json())  # noqa: E501
--- transformers/cmap/python-flask-server/openapi_server/test/test_transformers_controller.py
+++ transformers/cmap/python-flask-server/openapi_server/test/test_transformers_controller.py
@@ -6,9 +6,8 @@ import unittest
 from flask import json
 from six import BytesIO
 
-from openapi_server.models.compound_info import CompoundInfo  # noqa: E501
+from openapi_server.models.element import Element  # noqa: E501
 from openapi_server.models.error_msg import ErrorMsg  # noqa: E501
-from openapi_server.models.gene_info import GeneInfo  # noqa: E501
 from openapi_server.models.transformer_info import TransformerInfo  # noqa: E501
 from openapi_server.models.transformer_query import TransformerQuery  # noqa: E501
 from openapi_server.test import BaseTestCase
@@ -17,8 +16,8 @@ from openapi_server.test import BaseTestCase
 class TestTransformersController(BaseTestCase):
     """TransformersController integration test stubs"""
 
-    def test_input_class_compound_transform_post(self):
-        """Test case for input_class_compound_transform_post
+    def test_input_class_output_class_transform_post(self):
+        """Test case for input_class_output_class_transform_post
 
         Transform a list of genes or compounds
         """
@@ -28,26 +27,7 @@ class TestTransformersController(BaseTestCase):
             'Content-Type': 'application/json',
         }
         response = self.client.open(
-            '/cmap/{input_class}/compound/transform'.format(input_class='input_class_example'),
-            method='POST',
-            headers=headers,
-            data=json.dumps(transformer_query),
-            content_type='application/json')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-
-    def test_input_class_gene_transform_post(self):
-        """Test case for input_class_gene_transform_post
-
-        Transform a list of genes or compounds
-        """
-        transformer_query = {}
-        headers = { 
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        }
-        response = self.client.open(
-            '/cmap/{input_class}/gene/transform'.format(input_class='input_class_example'),
+            '/cmap/{input_class}/{output_class}/transform'.format(input_class='input_class_example', output_class='output_class_example'),
             method='POST',
             headers=headers,
             data=json.dumps(transformer_query),
