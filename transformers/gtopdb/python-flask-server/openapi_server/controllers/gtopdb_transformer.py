14d49f2913d43dc0e1206d73d99d6f397e3e0e75
michelle-mantilla <68681297+michelle-mantilla@users.noreply.github.com>
2021-01-15 13:53:41 -0500

    Update gtopdb_transformer.py
    Fixed target transformer to allow compounds with no pubchem ID to be passed

--- transformers/gtopdb/python-flask-server/openapi_server/controllers/gtopdb_transformer.py
+++ transformers/gtopdb/python-flask-server/openapi_server/controllers/gtopdb_transformer.py
@@ -252,48 +252,49 @@ class GtoPdbTargetsTransformer(Transformer):
         gene_list = []
         genes = {}
         for compound in compound_list:
-            if compound.identifiers['pubchem'] is not None:
-                cid= self.get_pubchemCID(compound)
-                query = """
-                SELECT DISTINCT
-                    TARGET_ID,
-                    INTERACTION.INTERACTION_ID, 
-                    INTERACTION.TARGET_SPECIES, 
-                    INTERACTION.LIGAND_ID,
-                    INTERACTION.TYPE, 
-                    INTERACTION.ACTION, 
-                    INTERACTION.ACTION_COMMENT, 
-                    INTERACTION.SELECTIVITY, 
-                    INTERACTION.ENDOGENOUS, 
-                    INTERACTION.PRIMARY_TARGET,
-                    INTERACTION.CONCENTRATION_RANGE, 
-                    INTERACTION.AFFINITY_UNITS, 
-                    INTERACTION.AFFINITY_HIGH, 
-                    INTERACTION.AFFINITY_MEDIAN, 
-                    INTERACTION.AFFINITY_LOW, 
-                    INTERACTION.ORIGINAL_AFFINITY_UNITS,
-                    INTERACTION.ORIGINAL_AFFINITY_LOW_NM,
-                    INTERACTION.ORIGINAL_AFFINITY_MEDIAN_NM,
-                    INTERACTION.ORIGINAL_AFFINITY_HIGH_NM,
-                    INTERACTION.ORIGINAL_AFFINITY_RELATION, 
-                    INTERACTION.ASSAY_DESCRIPTION,
-                    INTERACTION.RECEPTOR_SITE, 
-                    INTERACTION.LIGAND_CONTEXT, 
-                    INTERACTION.PUBMED_ID    
-                FROM LIGAND
-                JOIN INTERACTION ON LIGAND.LIGAND_ID = INTERACTION.LIGAND_ID
-                WHERE LIGAND.PUBCHEMCID = ?;
-                """
-                cur=connection.execute(query,(cid,))
-                for row in cur.fetchall():
-                    target_id= row["TARGET_ID"]
-                    if target_id not in genes:   
-                        target= self.get_target(target_id)[0]
-                        gene_list.append(target)
-                        genes[target_id]= target
-                    target= genes[target_id]
-                    # add connection element here by calling add_connection function
-                    self.add_connections(row, target, compound)
+            if compound.identifiers.get('pubchem') == None:
+                continue
+            cid= self.get_pubchemCID(compound)
+            query = """
+            SELECT DISTINCT
+                TARGET_ID,
+                INTERACTION.INTERACTION_ID, 
+                INTERACTION.TARGET_SPECIES, 
+                INTERACTION.LIGAND_ID,
+                INTERACTION.TYPE, 
+                INTERACTION.ACTION, 
+                INTERACTION.ACTION_COMMENT, 
+                INTERACTION.SELECTIVITY, 
+                INTERACTION.ENDOGENOUS, 
+                INTERACTION.PRIMARY_TARGET,
+                INTERACTION.CONCENTRATION_RANGE, 
+                INTERACTION.AFFINITY_UNITS, 
+                INTERACTION.AFFINITY_HIGH, 
+                INTERACTION.AFFINITY_MEDIAN, 
+                INTERACTION.AFFINITY_LOW, 
+                INTERACTION.ORIGINAL_AFFINITY_UNITS,
+                INTERACTION.ORIGINAL_AFFINITY_LOW_NM,
+                INTERACTION.ORIGINAL_AFFINITY_MEDIAN_NM,
+                INTERACTION.ORIGINAL_AFFINITY_HIGH_NM,
+                INTERACTION.ORIGINAL_AFFINITY_RELATION, 
+                INTERACTION.ASSAY_DESCRIPTION,
+                INTERACTION.RECEPTOR_SITE, 
+                INTERACTION.LIGAND_CONTEXT, 
+                INTERACTION.PUBMED_ID    
+            FROM LIGAND
+            JOIN INTERACTION ON LIGAND.LIGAND_ID = INTERACTION.LIGAND_ID
+            WHERE LIGAND.PUBCHEMCID = ?;
+            """
+            cur=connection.execute(query,(cid,))
+            for row in cur.fetchall():
+                target_id= row["TARGET_ID"]
+                if target_id not in genes:   
+                    target= self.get_target(target_id)[0]
+                    gene_list.append(target)
+                    genes[target_id]= target
+                target= genes[target_id]
+                # add connection element here by calling add_connection function
+                self.add_connections(row, target, compound)
         return gene_list
     
 
@@ -468,7 +469,7 @@ class GtoPdbInhibitorsTransformer(Transformer):
         compounds = {}
         for gene in gene_list:
             # Only search if gene has HGNC id
-            if 'hgnc' in gene.identifiers:
+            if 'hgnc' in gene.identifiers and gene.identifiers['hgnc'] is not None:
                 hgnc= self.get_hgnc(gene)
                 query = """
                 SELECT DISTINCT
