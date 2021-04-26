d8931703ec361507652375d34e73e3beeb885d24
vdancik <vdancik@broadinstitute.org>
2021-04-26 18:14:56 -0400

    update version number

--- transformers/inxight_drugs/python-flask-server/openapi_server/__main__.py
+++ transformers/inxight_drugs/python-flask-server/openapi_server/__main__.py
@@ -4,13 +4,13 @@ import connexion
 
 from openapi_server import encoder
 
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'Transformer API for Inxight_Drugs'},
+            pythonic_params=True)
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Transformer API for Inxight_Drugs'},
-                pythonic_params=True)
     app.run(port=8360,debug=True)
 
 
