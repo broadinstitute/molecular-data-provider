b36f746c64a2c728edc82921acf01cefaf5b8f31
Vlado Dancik <vdancik@broadinstitute.org>
2021-09-29 12:23:13 -0400

    DepMap transformer version 2.3.0
    Co-Authored-By: michelle-mantilla <68681297+michelle-mantilla@users.noreply.github.com>

--- transformers/depmap/python-flask-server/openapi_server/__main__.py
+++ transformers/depmap/python-flask-server/openapi_server/__main__.py
@@ -4,14 +4,14 @@ import connexion
 
 from openapi_server import encoder
 
-
-def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
                 arguments={'title': 'Transformer API for DepMap gene-knockout correlations'},
                 pythonic_params=True)
-    app.run(port=8080)
+
+def main():
+    app.run(port=8010, debug= True)
 
 
 if __name__ == '__main__':
--- transformers/depmap/python-flask-server/openapi_server/controllers/transformers_controller.py
+++ transformers/depmap/python-flask-server/openapi_server/controllers/transformers_controller.py
@@ -1,3 +1,4 @@
+from openapi_server.controllers.depmap_transformer import DepMapExpander
 import connexion
 import six
 
@@ -6,9 +7,13 @@ from openapi_server.models.error_msg import ErrorMsg  # noqa: E501
 from openapi_server.models.transformer_info import TransformerInfo  # noqa: E501
 from openapi_server.models.transformer_query import TransformerQuery  # noqa: E501
 from openapi_server import util
+from openapi_server.controllers.depmap_transformer import DepMapExpander
 
+transformer = {
+    'correlation': DepMapExpander()
+}
 
-def service_transform_post(service, transformer_query, cache=None):  # noqa: E501
+def service_transform_post(service, body, cache=None):  # noqa: E501
     """Transform a list of genes or compounds
 
     Depending on the function of a transformer, creates, expands, or filters a list. # noqa: E501
@@ -24,7 +29,7 @@ def service_transform_post(service, transformer_query, cache=None):  # noqa: E50
     """
     if connexion.request.is_json:
         transformer_query = TransformerQuery.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return transformer[service].transform(transformer_query)
 
 
 def service_transformer_info_get(service, cache=None):  # noqa: E501
@@ -39,4 +44,4 @@ def service_transformer_info_get(service, cache=None):  # noqa: E501
 
     :rtype: TransformerInfo
     """
-    return 'do some magic!'
+    return transformer[service].transformer_info(cache)
