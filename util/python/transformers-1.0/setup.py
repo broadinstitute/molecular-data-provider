1d8ac6037f58ec88987f00cd1bae334237ef4950
vdancik <vdancik@users.noreply.github.com>
2021-10-11 16:32:32 -0400

    keep track of versions of transformer base class

--- util/python/transformers-1.0/setup.py
+++ util/python/transformers-1.0/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'base-transformer',
-    version = '2.0.0',
+    version = '1.0.0',
     author_email="translator@broadinstitute.org",
     packages=find_packages(),
     description="Base transformer class"
