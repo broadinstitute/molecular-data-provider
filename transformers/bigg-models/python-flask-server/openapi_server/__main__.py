72a6bb758bbb309e080576206db5d315e76b5280
Vlado Dancik <vdancik@broadinstitute.org>
2021-10-19 18:41:24 -0400

    BiGG Models transformer ver 3.2.0
    Co-Authored-By: Larry Chung <codewarrior2000@users.noreply.github.com>

--- transformers/bigg-models/python-flask-server/openapi_server/__main__.py
+++ transformers/bigg-models/python-flask-server/openapi_server/__main__.py
@@ -4,14 +4,15 @@ import connexion
 
 from openapi_server import encoder
 
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'Transformer API for BiGG Models transformer'},
+            pythonic_params=True)
+
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Transformer API for BiGG Models transformer'},
-                pythonic_params=True)
-    app.run(port=8080)
+    app.run(port=8420)
 
 
 if __name__ == '__main__':
--- transformers/bigg-models/python-flask-server/openapi_server/controllers/transformers_controller.py
+++ transformers/bigg-models/python-flask-server/openapi_server/controllers/transformers_controller.py
@@ -7,8 +7,23 @@ from openapi_server.models.transformer_info import TransformerInfo  # noqa: E501
 from openapi_server.models.transformer_query import TransformerQuery  # noqa: E501
 from openapi_server import util
 
+from openapi_server.controllers.bigg_transformer import BiGG_Compound_Producer  # noqa: E501
+from openapi_server.controllers.bigg_transformer import BiGG_Compound_Reactions_Transformer # noqa: E501
+from openapi_server.controllers.bigg_transformer import BiGG_Compound_Gene_Transformer # noqa: E501
+from openapi_server.controllers.bigg_transformer import BiGG_Gene_Reaction_Transformer # noqa: E501
 
-def service_transform_post(service, transformer_query, cache=None):  # noqa: E501
+#  According to openapi.yaml
+#   - metabolites
+#   - reactions
+#   - genes
+#   - gene-reactions
+transformer = { 'metabolites': BiGG_Compound_Producer(),
+                'reactions': BiGG_Compound_Reactions_Transformer(),
+                'genes': BiGG_Compound_Gene_Transformer(),
+                'gene-reactions': BiGG_Gene_Reaction_Transformer()
+}
+
+def service_transform_post(service, body, cache=None):  # noqa: E501
     """Transform a list of genes or compounds
 
     Depending on the function of a transformer, creates, expands, or filters a list. # noqa: E501
@@ -24,7 +39,7 @@ def service_transform_post(service, transformer_query, cache=None):  # noqa: E50
     """
     if connexion.request.is_json:
         transformer_query = TransformerQuery.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return transformer[service].transform(transformer_query)
 
 
 def service_transformer_info_get(service, cache=None):  # noqa: E501
@@ -39,4 +54,4 @@ def service_transformer_info_get(service, cache=None):  # noqa: E501
 
     :rtype: TransformerInfo
     """
-    return 'do some magic!'
+    return transformer[service].info
