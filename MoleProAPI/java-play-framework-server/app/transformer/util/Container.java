33f52de25606c9fa8bb13360297b31b1892200e5
devisettysaivikas <devisettysaivikas97@gmail.com>
2021-11-01 13:25:12 -0400

    remove unrelated files

--- MoleProAPI/java-play-framework-server/app/transformer/util/Container.java
+++
@@ -1,6 +0,0 @@
-package transformer.util;
-
-public interface Container<S> {
-
-	boolean contains(final S key);
-}
--- MoleProDB/builder/src/org/broadinstitute/translator/moleprodb/db/ChemStructureAttributeTable.java
+++
@@ -1,9 +0,0 @@
-package org.broadinstitute.translator.moleprodb.db;
-
-public class ChemStructureAttributeTable extends AttributeMapTable {
-
-	public ChemStructureAttributeTable(MoleProDB db) {
-		super(db, "Chem_Structure_Attribute", "structure_id");
-	}
-
-}
