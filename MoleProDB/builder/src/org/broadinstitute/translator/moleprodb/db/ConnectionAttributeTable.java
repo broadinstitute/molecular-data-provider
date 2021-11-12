b91571ba412808896346969e6e9486d295743685
devisettysaivikas <devisettysaivikas97@gmail.com>
2021-11-12 11:30:28 -0500

    sync

--- MoleProDB/builder/src/org/broadinstitute/translator/moleprodb/db/ConnectionAttributeTable.java
+++
@@ -1,9 +0,0 @@
-package org.broadinstitute.translator.moleprodb.db;
-
-public class ConnectionAttributeTable extends AttributeMapTable {
-
-	public ConnectionAttributeTable(MoleProDB db) {
-		super(db, "Connection_Attribute", "connection_id");
-	}
-
-}
--- transformers/node-normalizer/python-flask-server/util/scala/xml/src/org/broadinstitute/translator/xml/ChildBuilder.scala
+++
@@ -1,7 +0,0 @@
-package org.broadinstitute.translator.xml
-
-class ChildBuilder[T <: Builder](override val tag: String, val child: T) extends Builder {
-  override def getBuilder(tag: String, attr: Map[String, String]): Option[Builder] = 
-    if (child.matches(tag, attr)) Some(child) else None 
-}
-
