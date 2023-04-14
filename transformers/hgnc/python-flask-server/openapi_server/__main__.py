fe863215c58d5cd29e1d1a28b149b5ebec06f8af
vdancik <vdancik@broadinstitute.org>
2023-04-14 15:13:00 -0400

    HGNC transformer API version 2.5

--- transformers/hgnc/python-flask-server/openapi_server/__main__.py
+++ transformers/hgnc/python-flask-server/openapi_server/__main__.py
@@ -5,14 +5,15 @@ import connexion
 from openapi_server import encoder
 
 
-def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Transformer API for HGNC'},
-                pythonic_params=True)
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'Transformer API for HGNC'},
+            pythonic_params=True)
 
-    app.run(port=8080)
+
+def main():
+    app.run(port=8490, debug=True)
 
 
 if __name__ == '__main__':
