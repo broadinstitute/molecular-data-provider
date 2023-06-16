56b71149d972b370fd464ce9770eb9d4f464e8ad
vdancik <vdancik@broadinstitute.org>
2023-06-16 17:33:28 -0400

    Merge branch 'util-python'

--- util/python/transformers/transformers/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name = 'base-transformer',
-    version = '2.0.0',
-    author_email="translator@broadinstitute.org",
-    packages=find_packages(),
-    description="Base transformer class"
-)
