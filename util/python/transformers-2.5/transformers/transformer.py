02e834dd8f4fd56d5a694cb0ac74a604546955fe
vdancik <vdancik@broadinstitute.org>
2023-12-20 12:13:33 -0500

    do not ignore name's language and add support for infores

--- util/python/transformers-2.5/transformers/transformer.py
+++ util/python/transformers-2.5/transformers/transformer.py
@@ -334,7 +334,7 @@ class Transformer:
             name_type=type, 
             source=name_source if name_source is not None else self.SOURCE, 
             provided_by=self.PROVIDED_BY, 
-            language=None
+            language=language
         )
 
 
@@ -399,6 +399,8 @@ class TransformerUtility:
                         else:
                             prefix["molepro_prefix"] = line['MolePro CURIE prefix']
                         prefix["biolink_prefix"] = line['Biolink CURIE prefix']
+                        if line['infores'] != '':
+                            prefix['infores'] = line['infores']
                         prefixMap[line['Biolink class']][line['MolePro field name']]= prefix
                         
 #       Save as a JSON file
