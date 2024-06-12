90ebd3f29c66946e469cfbea11dd510a9d5001f9
vdancik <vdancik@broadinstitute.org>
2024-06-12 18:06:26 -0400

    autogenerate python-flask server for ChemBank transformer ver 2.5

--- transformers/chembank/python-flask-server/openapi_server/controllers/transformers_controller.py
+++ transformers/chembank/python-flask-server/openapi_server/controllers/transformers_controller.py
@@ -1,5 +1,8 @@
 import connexion
 import six
+from typing import Dict
+from typing import Tuple
+from typing import Union
 
 from openapi_server.models.element import Element  # noqa: E501
 from openapi_server.models.error_msg import ErrorMsg  # noqa: E501
@@ -20,7 +23,7 @@ def service_transform_post(service, transformer_query, cache=None):  # noqa: E50
     :param cache: Directive for handling caching, can be &#39;yes&#39; (default), &#39;no&#39;, &#39;bypass&#39; or &#39;remove&#39;
     :type cache: str
 
-    :rtype: List[Element]
+    :rtype: Union[List[Element], Tuple[List[Element], int], Tuple[List[Element], int, Dict[str, str]]
     """
     if connexion.request.is_json:
         transformer_query = TransformerQuery.from_dict(connexion.request.get_json())  # noqa: E501
@@ -37,6 +40,6 @@ def service_transformer_info_get(service, cache=None):  # noqa: E501
     :param cache: Directive for handling caching, can be &#39;yes&#39; (default), &#39;no&#39;, &#39;bypass&#39; or &#39;remove&#39;
     :type cache: str
 
-    :rtype: TransformerInfo
+    :rtype: Union[TransformerInfo, Tuple[TransformerInfo, int], Tuple[TransformerInfo, int, Dict[str, str]]
     """
     return 'do some magic!'
--- transformers/chembank/python-flask-server/setup.py
+++ transformers/chembank/python-flask-server/setup.py
@@ -22,10 +22,10 @@ REQUIRES = [
 setup(
     name=NAME,
     version=VERSION,
-    description="Transformer API for Molecular Data Provider",
+    description="Transformer API for ChemBank",
     author_email="translator@broadinstitute.org",
     url="",
-    keywords=["OpenAPI", "Transformer API for Molecular Data Provider"],
+    keywords=["OpenAPI", "Transformer API for ChemBank"],
     install_requires=REQUIRES,
     packages=find_packages(),
     package_data={'': ['openapi/openapi.yaml']},
