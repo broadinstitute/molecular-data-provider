904b58f2a27079216f15713be055f250dfbf1d60
vdancik <vdancik@broadinstitute.org>
2024-10-02 11:25:40 -0400

    Reactome transformer 2.5.0
    Co-Authored-By: Larry Chung <codewarrior2000@users.noreply.github.com>

--- transformers/reactome/python-flask-server/openapi_server/__main__.py
+++ transformers/reactome/python-flask-server/openapi_server/__main__.py
@@ -4,15 +4,15 @@ import connexion
 
 from openapi_server import encoder
 
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'Transformer API for Reactome'},
+            pythonic_params=True)
+
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Transformer API for Reactome'},
-                pythonic_params=True)
-
-    app.run(port=8080)
+    app.run(port=8510, debug=True)
 
 
 if __name__ == '__main__':
--- transformers/reactome/python-flask-server/openapi_server/controllers/transformers_controller.py
+++ transformers/reactome/python-flask-server/openapi_server/controllers/transformers_controller.py
@@ -10,8 +10,29 @@ from openapi_server.models.transformer_info import TransformerInfo  # noqa: E501
 from openapi_server.models.transformer_query import TransformerQuery  # noqa: E501
 from openapi_server import util
 
+from openapi_server.controllers.reactome_transformer import ReactomeEntityProducer
+from openapi_server.controllers.reactome_transformer import ReactomeReactionProducer
+from openapi_server.controllers.reactome_transformer import ReactomePathwayProducer
+from openapi_server.controllers.reactome_transformer import ReactomeComplexProducer
+from openapi_server.controllers.reactome_transformer import ReactomeInteractionTransformer
+from openapi_server.controllers.reactome_transformer import ReactomeReactionTransformer
+from openapi_server.controllers.reactome_transformer import ReactomeComplexTransformer
+from openapi_server.controllers.reactome_transformer import ReactomePathwayTransformer
 
-def service_transform_post(service, transformer_query, cache=None):  # noqa: E501
+transformer = {
+    'entity-producer': ReactomeEntityProducer(),
+    'reaction-producer': ReactomeReactionProducer(),
+    'pathway-producer': ReactomePathwayProducer(),
+    'complex-producer': ReactomeComplexProducer(),
+    'interaction': ReactomeInteractionTransformer(),
+    'reaction': ReactomeReactionTransformer(),
+    'complex': ReactomeComplexTransformer(),
+    'pathway': ReactomePathwayTransformer()
+    
+}
+
+
+def service_transform_post(service, body, cache=None):  # noqa: E501
     """Transform a list of genes or compounds
 
     Depending on the function of a transformer, creates, expands, or filters a list. # noqa: E501
@@ -27,7 +48,8 @@ def service_transform_post(service, transformer_query, cache=None):  # noqa: E50
     """
     if connexion.request.is_json:
         transformer_query = TransformerQuery.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    #return 'do some magic!'
+    return transformer[service].transform(transformer_query)
 
 
 def service_transformer_info_get(service, cache=None):  # noqa: E501
@@ -42,4 +64,5 @@ def service_transformer_info_get(service, cache=None):  # noqa: E501
 
     :rtype: Union[TransformerInfo, Tuple[TransformerInfo, int], Tuple[TransformerInfo, int, Dict[str, str]]
     """
-    return 'do some magic!'
+    #return 'do some magic!'
+    return transformer[service].transformer_info(cache)
